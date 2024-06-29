c33e64c8eb5ea70c63c3154baecbe4148c8efb49
戒尺 <itxiao6@qq.com>
2024-06-29 10:17:48 +0800

    驱动根据设置开启状态来选择

--- src/functions.php
+++ src/functions.php
@@ -1,14 +1,19 @@
 <?php
+
 use Slowlyo\OwlAdmin\Admin;
 
 
-if(!function_exists('ManoImageControl')){
-    function ManoImageControl(string $name = '', string $label = '',string $disk = 'local'){
+if (!function_exists('ManoImageControl')) {
+    function ManoImageControl(string $name = '', string $label = '')
+    {
+        $disk = \ManoCode\FileSystem\Models\FilesystemConfig::query()->where('state', 1)->value('key');
         return amis()->ImageControl($name, $label)->receiver("/mano-code/upload/{$disk}/upload-image");
     }
 }
-if(!function_exists('ManoFileControl')){
-    function ManoFileControl(string $name = '', string $label = '',string $disk = 'local'){
+if (!function_exists('ManoFileControl')) {
+    function ManoFileControl(string $name = '', string $label = '')
+    {
+        $disk = \ManoCode\FileSystem\Models\FilesystemConfig::query()->where('state', 1)->value('key');
         return amis()->FileControl($name, $label)->receiver("/mano-code/upload/{$disk}/upload-file")->
         startChunkApi("/mano-code/upload/{$disk}/upload_chunk_start")->
         chunkApi("/mano-code/upload/{$disk}/upload_chunk")->
@@ -16,8 +21,10 @@ if(!function_exists('ManoFileControl')){
     }
 }
 
-if(!function_exists('ManoRichTextControl')){
-    function ManoRichTextControl(string $name = '', string $label = '',string $disk = 'local'){
+if (!function_exists('ManoRichTextControl')) {
+    function ManoRichTextControl(string $name = '', string $label = '')
+    {
+        $disk = \ManoCode\FileSystem\Models\FilesystemConfig::query()->where('state', 1)->value('key');
         return amis()->RichTextControl($name, $label)->receiver("/mano-code/upload/{$disk}/upload-image");
     }
 }
